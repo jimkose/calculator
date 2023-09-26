185f4539e2d688b95f2c80d6be2e932f2e45ab66
Jim Kose <jimkose496@gmail.com>
2023-09-26 15:11:08 -0400

    add ability to do addition, subtraction, multiplication and division using buttons

--- scripts.js
+++ scripts.js
@@ -1,33 +1,29 @@
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide(a, b) {
-    return a / b
+    return Number(a) / Number(b);
 }
 
 function operate(a, b, operator) {
     switch (operator) {
         case '+':
-            add(a, b);
-            break;
+            return add(a, b);
         case '-':
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case '*':
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case '/':
-            divide(a, b);
-            break;
+            return divide(a, b);
         default:
             console.log(`Error. Unknown operator: ${operator}`);
     }
@@ -38,30 +34,56 @@ function populateDisplay(number) {
     display.textContent = number;
 }
 
+function wipeDisplay(option) {
+    const operators = document.querySelectorAll('.operator');
+
+}
+
 // variables used to hold numbers involved in current calculation
 // a is the first number, b is the second number, operator holds the symbol for the operation
-let a;
-let b;
-let operator;
+let a = null;
+let b = null;
+let operator = null;
 // display is the number currently shown on screen
 let display = 0;
+// keeps track of last key pressed. used to determine if screen should be wiped after an operator is displayed
+let lastKey = null;
 
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(button => {
     button.addEventListener('click', event => {
         let display = document.querySelector('.display').innerText;
-        if (display === '0') display = event.target.innerText;
+        if (display === '0' || lastKey !== 'number') display = event.target.innerText;
         else display += event.target.innerText;
         populateDisplay(display);
+        lastKey = 'number';
     });
 });
 
 const operators = document.querySelectorAll('.operator');
 operators.forEach(button => {
     button.addEventListener('click', event => {
-        a = document.querySelector('.display').innerText;
+        if (lastKey === 'operator') {
+            operator = event.target.dataset.value;
+            return;
+        };
+        
+        lastKey = 'operator';
+
+        if (a === null || lastKey === 'equals') {
+            a = document.querySelector('.display').innerText;
+            operator = event.target.dataset.value;
+            return;
+        }
+        
+        const display = document.querySelector('.display');
+        b = display.innerText;
+        const result = operate(a, b, operator);
+        display.innerText = result;
+
+        a = result;
+        b = null;
         operator = event.target.dataset.value;
-        populateDisplay(event.target.innerText);
     });
 });
 
@@ -69,7 +91,23 @@ const clearAll = document.querySelector('#clear-all');
 clearAll.addEventListener('click', event => {
     const display = document.querySelector('.display');
     display.innerText = 0;
-    a = 0;
-    b = 0;
-    operator = 0;
-})
+    a = null;
+    b = null;
+    operator = null;
+    lastKey = null;
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', eventu => {
+    if (lastKey !== 'number' || lastKey === null) return;
+    
+    const display = document.querySelector('.display');
+    b = display.innerText;
+    const result = operate(a, b, operator);
+    display.innerText = result;
+    
+    a = null;
+    b = null;
+    operator = null;
+    lastKey = 'equals';
+});
